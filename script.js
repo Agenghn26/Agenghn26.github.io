5192f613ff99c0c448378beddcfb4c9748b3bb78
Agenghn26 <nugrohoageng07@gmail.com>
2025-03-18 23:05:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(() => {
-        document.getElementById("book").innerText = "Oops!";
-    }, 2500);
+document.addEventListener("DOMContentLoaded", function () {
+    const rainContainer = document.querySelector(".rain");
+    
+    function createRaindrop() {
+        const drop = document.createElement("div");
+        drop.classList.add("drop");
+        
+        drop.style.left = Math.random() * 100 + "vw";
+        drop.style.animationDuration = (Math.random() * 1.5 + 0.5) + "s";
+        
+        rainContainer.appendChild(drop);
+        
+        setTimeout(() => {
+            drop.remove();
+        }, 2000);
+    }
+    
+    setInterval(createRaindrop, 100);
 });
-
